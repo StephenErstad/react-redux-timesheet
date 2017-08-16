1f9a87e05413a8aab2508a85720f577717cc43a3
Stephen Erstad <Stephen.Erstad@Target.com>
2017-08-16 11:31:10 -0500

    FInished solution

--- src/components/employees/EmployeeTable.test.js
+++ src/components/employees/EmployeeTable.test.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import EmployeeTable from './EmployeeTable';
-import { mount } from 'enzyme';
+import {mount} from 'enzyme';
 
-describe('Employee Table Component: ', function () {
+describe('Employee Table Component: ', function() {
 
-  it('implement me', function () {
+  it('implement me', function() {
     expect(true).toBe(true);
   });
 
-});
+  it('should instantiate the Employee Table', function() {
+
+    const employees = [
+      {
+        username: 'fflintstone',
+        'email': 'fred.flintstone@slatequarry.com',
+        'firstName': 'Fred',
+        'lastName': 'Flintstone',
+        'admin': true,
+        '_id': 1
+      }];
+
+    const component = mount(
+        <EmployeeTable employees={employees}/>
+    );
+
+    expect(component).toContainReact(<th>Last Name</th>);
+    expect(component).toIncludeText('Flintstone');
+
+    expect(component.find('tbody tr')).toHaveLength(1);
+
+  });
+
+})
+;
--- src/components/employees/Employees.test.js
+++ src/components/employees/Employees.test.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import Employees from './Employees';
-import { shallow, mount } from 'enzyme';
+import {shallow, mount} from 'enzyme';
 
-describe('Employees Component: ', function () {
+describe('Employees Component: ', function() {
 
-  it('implement me', function () {
+  it('implement me', function() {
     expect(true).toBe(true);
   });
 
+  it('should instantiate the Employee Component', function() {
+    const component = shallow(
+        <Employees/>
+    );
+
+    expect(component).toHaveLength(1);
+
+  });
+
+  it('should contain a correct employee', function() {
+    const component = mount(
+        <Employees/>
+    );
+
+    expect(component).toIncludeText('admin@mixtape.com');
+  });
 });
