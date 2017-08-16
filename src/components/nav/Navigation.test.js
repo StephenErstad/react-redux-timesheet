2376f8096b15f7aad8cec46fa87aca36698fda29
Stephen Erstad <Stephen.Erstad@Target.com>
2017-08-16 11:17:15 -0500

    Partial solution

--- src/components/nav/Navigation.test.js
+++ src/components/nav/Navigation.test.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Navigation from './Navigation';
-import { mount } from 'enzyme';
+import {mount} from 'enzyme';
 
 import {BrowserRouter} from 'react-router-dom';
 
-describe('Navigation Component: ', function () {
+describe('Navigation Component: ', function() {
 
-  it('implement me', function () {
-    expect(true).toBe(true);
+  let nav;
+
+  beforeEach(() => {
+    nav = mount(<BrowserRouter><Navigation/></BrowserRouter>);
+  });
+
+  it('should instantiate the Navigation Component', function() {
+    expect(nav).toHaveLength(1);
   });
 
 });
